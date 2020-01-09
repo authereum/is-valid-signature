b244adef7b9908eb315c5029626ce3fd36c8da78
Shane Fontaine <shane6fontaine@gmail.com>
2020-01-09 14:47:24 -0800

    Fix message type bug

--- test/isValidSignature.test.js
+++ test/isValidSignature.test.js
@@ -1,6 +1,5 @@
 const isValidSignature = require('..')
 
-const { fromRpcSig, toBuffer, ecrecover } = require('ethereumjs-util')
 const { expect } = require('chai')
 const utils = require('@authereum/utils')
 const web3Utils = require('web3-utils')
@@ -9,9 +8,10 @@ const AccountMock = artifacts.require('AccountMock')
 
 contract('isValidSignature', (accounts) => {
 
-  let signer, message, messageHash, messageSignature, messageHashSignature
-  beforeEach(async () => {
+  let signer, message, messageHash, messageSignature, messageHashSignature, badMessage, badMessageHash, badMessageSignature, badMessageHashSignature
+  before(async () => {
     signer = accounts[0]
+
     message = "Hello world"
     messageHash = await utils.soliditySha3(message)
 
@@ -20,9 +20,20 @@ contract('isValidSignature', (accounts) => {
 
     messageHashSignature = await web3.eth.sign(messageHash, signer)
     messageHashSignature = normalizeSignature(messageHashSignature)
+
+    // Bad messages for cases that return false
+    badMessage = "Bad message"
+    badMessageHash = await utils.soliditySha3(badMessage)
+
+    badMessageSignature = await web3.eth.sign(badMessage, signer)
+    badMessageSignature = normalizeSignature(badMessageSignature)
+
+    badMessageHashSignature = await web3.eth.sign(badMessageHash, signer)
+    badMessageHashSignature = normalizeSignature(badMessageHashSignature)
   })
 
   describe('when signing a hash', () => {
+    // true cases
     it('should check externally owned account signature', async () => {
       expect(await isValidSignature(signer, messageHash, messageHashSignature, web3)).to.equal(true)
     })
@@ -31,9 +42,29 @@ contract('isValidSignature', (accounts) => {
       contract = await AccountMock.new(signer)
       expect(await isValidSignature(contract.address, messageHash, messageHashSignature, web3)).to.equal(true)
     })
+
+    // false cases
+    it('should return false for externally owned account signature with bad data', async () => {
+      expect(await isValidSignature(signer, badMessageHash, messageHashSignature, web3)).to.equal(false)
+    })
+
+    it('should return false for ERC1271 signature with bad data', async () => {
+      contract = await AccountMock.new(signer)
+      expect(await isValidSignature(contract.address, badMessageHash, messageHashSignature, web3)).to.equal(false)
+    })
+
+    it('should return false for externally owned account signature with bad signature', async () => {
+      expect(await isValidSignature(signer, messageHash, badMessageHashSignature, web3)).to.equal(false)
+    })
+
+    it('should return false for ERC1271 signature with bad signature', async () => {
+      contract = await AccountMock.new(signer)
+      expect(await isValidSignature(contract.address, messageHash, badMessageHashSignature, web3)).to.equal(false)
+    })
   })
 
   describe('when signing a string', () => {
+    // true cases
     it('should check externally owned account signature', async () => {
       expect(await isValidSignature(signer, message, messageSignature, web3)).to.equal(true)
     })
@@ -42,8 +73,26 @@ contract('isValidSignature', (accounts) => {
       contract = await AccountMock.new(signer)
       expect(await isValidSignature(contract.address, message, messageSignature, web3)).to.equal(true)
     })
-  })
 
+    // false cases
+    it('should return false for externally owned account signature with bad data', async () => {
+      expect(await isValidSignature(signer, badMessage, messageSignature, web3)).to.equal(false)
+    })
+
+    it('should return false for ERC1271 signature with bad data', async () => {
+      contract = await AccountMock.new(signer)
+      expect(await isValidSignature(contract.address, badMessage, messageSignature, web3)).to.equal(false)
+    })
+
+    it('should return false for externally owned account signature with bad signature', async () => {
+      expect(await isValidSignature(signer, message, badMessageSignature, web3)).to.equal(false)
+    })
+
+    it('should return false for ERC1271 signature with bad signature', async () => {
+      contract = await AccountMock.new(signer)
+      expect(await isValidSignature(contract.address, message, badMessageSignature, web3)).to.equal(false)
+    })
+  })
 })
 
 const normalizeSignature = (signature) => {
